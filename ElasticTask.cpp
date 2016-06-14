0534fdc5c81d2423eaff369adaab8cf07599e33a
Simon Bihel <simon.bihel@outlook.com>
2016-06-14 16:59:47 +0200

    Week 5 Day 2

---
+++ ElasticTask.cpp
@@ -0,0 +1,94 @@
+#include "ElasticTask.hpp"
+#include "simgrid/s4u/engine.hpp"
+#include "simgrid/s4u/comm.hpp"
+#include "simgrid/s4u/forward.hpp"
+#include <vector>
+#include "simgrid/msg.h"
+
+using namespace simgrid;
+using namespace s4u;
+
+//ElasticTask::ElasticTask(smx_process_t smx_proc) : Actor::Actor(smx_proc) { }
+
+ElasticTask::ElasticTask(const char *name, s4u::Host *host, std::function<void()> code, msg_task_t _task)
+        : Actor::Actor(name, host, std::move(code)) {
+    task = _task;
+}
+//ElasticTask::ElasticTask(const char *name, s4u::Host *host, C code)
+//        : Actor::Actor(name, host, code) {}
+
+ElasticTask::~ElasticTask() {
+    Actor::~Actor();
+}
+
+
+//namespace this_task {
+    void ElasticTask::setTriggerRatioVariation(std::vector<ratioChange> fluctuations) {
+        ratioFluctuations = fluctuations;
+        for (std::vector<ratioChange>::iterator it = fluctuations.begin(); it != fluctuations.end(); ++it) {
+            if ((*it).time <= (Engine::instance())->getClock()) {
+                currentRatio = (*it).visitsPerSec;
+                currentIndex = it - fluctuations.begin();
+                break;
+            }
+        }
+    }
+
+    void ElasticTask::addOutputStream(ElasticTask e2) {
+        outputStreams.push_back(e2);
+    }
+
+    void ElasticTask::modifyTask(msg_task_t _task) {
+        task = _task;
+
+    }
+//void ElasticTask::modifyTask(C code) {}
+
+//    void ElasticTask::modifyHost(s4u::Host *host, std::function<void()> code) {
+//        this->set_pimpl(simcall_process_create(this->getName(),std::move(code),nullptr,host->name().c_str(),-1,nullptr,
+//                                               0));
+//    }
+
+    void ElasticTask::updateTriggerRatioVariation(std::vector <ratioChange> fluctuations) {
+        ratioFluctuations = fluctuations;
+    }
+
+    void ElasticTask::setTriggerRatio(double ratio) {
+        currentRatio = ratio;
+        ratioFluctuations.clear();
+    }
+
+    void ElasticTask::triggerOneTime() {
+//        double flops = this->getHost().getAvailableFlops();  // TODO, manage available flops
+        MSG_task_execute(task);
+    }
+
+    void ElasticTask::execute() {
+//        double flops = this->getHost().getAvailableFlops();  // TODO, manage available flops
+        while (1) {
+            for (int i = 0; i < currentRatio; i++) {  // TODO, for now it acts like currentRation is an int
+                MSG_task_execute(task);  // TODO, find a way to execute them on another thread or something
+                for (std::vector<ElasticTask>::iterator it = outputStreams.begin(); it != outputStreams.end(); it++) {
+                    it->triggerOneTime();
+                }
+            }
+            sleep(1);
+            if (!ratioFluctuations.empty()) {
+                if (ratioFluctuations.end() - ratioFluctuations.begin() > currentIndex
+                    && ratioFluctuations.at(currentIndex + 1).visitsPerSec <= (Engine::instance())->getClock()) {
+                    currentRatio = ratioFluctuations.at(currentIndex + 1).visitsPerSec;
+                    currentIndex = currentIndex + 1;
+                }
+                if (ratioFluctuations.end() - ratioFluctuations.begin() == currentIndex && currentRatio == 0.0) {
+                    break;
+                }
+            }
+        }
+    }
+//void sleep(double duration);  // We probably should just change the visits ratio to 0.
+//void recv(Mailbox &chan);  // Is it useful ? Maybe for workflow
+//void send(Mailbox &chan, void*payload, size_t simulatedSize);  // Is it useful ? Maybe for workflow
+    void ElasticTask::kill() {
+        Actor::kill();
+    }  // I guess we don't have to delete the output tasks
+//}
--- ElasticTask.hpp
+++ ElasticTask.hpp
@@ -1,7 +1,20 @@
-#ifndef ELASTICTASK_HPP
-#define ELASTICTASK_HPP
+#ifndef SIMGRID_ELASTIC_TASK_H
+#define SIMGRID_ELASTIC_TASK_H
 
-#include <../simgrid/include/simgrid/s4u/actor.hpp>
+#include <vector>
+
+#include <xbt/base.h>
+#include <xbt/string.hpp>
+#include <xbt/signal.hpp>
+#include <xbt/Extendable.hpp>
+
+#include "simgrid/msg.h"
+
+#include <simgrid/simix.h>
+#include <simgrid/datatypes.h>
+#include <simgrid/s4u/forward.hpp>
+
+#include <simgrid/s4u/actor.hpp>
 
 typedef struct ratioChange {
     double time;
@@ -15,68 +28,68 @@ ratioChange rC(double time, double visitsPerSec) {
     return res;
 }
 
-using namespace simgrid;
-using namespace s4u;
+namespace simgrid {
+namespace s4u {
 
 /** @brief Elastic task.
  */
-XBT_PUBLIC_CLASS ElasticTask : public Actor {
-    explicit ElasticTask(smx_process_t smx_proc);
+    XBT_PUBLIC_CLASS ElasticTask : public Actor {
+//    explicit ElasticTask(smx_process_t smx_proc);
+    private:
+        std::vector<ratioChange> ratioFluctuations;
+        double currentRatio;
+        long currentIndex;
+//        double flopsTask;
+        std::vector<ElasticTask> outputStreams;
+        msg_task_t task;
     public:
-        ElasticTask(const char* name, s4u::Host *host, std::function<void()> code);  // no killTime
-        Elastictask(const char* name, s4u::Host *host, C code);
-        void ~ElasticTask();
+        ElasticTask(const char *name, s4u::Host *host, std::function<void()> code, msg_task_t _task);  // no killTime
+        template<class C>
+        ElasticTask(const char *name, s4u::Host *host, C code, msg_task_t _task)
+            : ElasticTask(name, host, std::function<void()>(std::move(code)), _task) { }
 
-        /** Set the variation of visits/triggers per seconds over the time. */
-        void setTriggerRatioVariation(vector<ratioChange> fluctuations);
-        //void setTriggerTrace(string fileName);
-        /** Add a link like in a workflow of tasks. */
+        ~ElasticTask();
+//        };
+
+//    namespace this_task {
+/** Set the variation of visits/triggers per seconds over the time. */
+        void setTriggerRatioVariation(std::vector<ratioChange> fluctuations);
+//void setTriggerTrace(string fileName);
+/** Add a link like in a workflow of tasks. */
         void addOutputStream(ElasticTask e2);
-        /** Modify the normal task that is triggered.
-         *
-         * You can use it to reduce the computational size for example.
+
+/** Modify the normal task that is triggered.
+ *
+ * You can use it to reduce the computational size for example.
+ */
+        void modifyTask(msg_task_t _task);
+
+        /** Change the host
          */
-        void modifyTask(task);
-        /** Update on the fly the fluctuation of visit/trigger ratio */
-        void updateTriggerRatioVariation(vector<date, ratio>);
-        /** Update on the fly the current visit/trigger ratio and delete the old vector of fluctuation. */
-        void setRatioVariation(date, ratio);
-        /** Trigger the inherent task one time.
-         *
-         * Could be used for the output stream. */
+//        void modifyHost(s4u::Host *host, std::function<void()> code);
+/** Update on the fly the fluctuation of visit/trigger ratio
+ *
+ * Keep the same time
+ */
+        void updateTriggerRatioVariation(std::vector<ratioChange> fluctuations);
+
+/** Update on the fly the current visit/trigger ratio and delete the old vector of fluctuation. */
+        void setTriggerRatio(double ratio);
+
+/** Trigger the inherent task one time.
+ *
+ * Could be used for the output stream. */
         void triggerOneTime();
-        /** Execute the elastic task until death.
-         *
-         * It will find itself the resources (e.g. flops) available.
-         */
+/** Execute the elastic task until death.
+ *
+ * It will find itself the resources (e.g. flops) available.
+ */
         void execute();
-        //void sleep(double duration);  // We probably should just change the visits ratio to 0.
-        //void recv(Mailbox &chan);  // Is it useful ? Maybe for workflow
-        //void send(Mailbox &chan, void*payload, size_t simulatedSize);  // Is it useful ? Maybe for workflow
-};
+//void sleep(double duration);  // We probably should just change the visits ratio to 0.
+//void recv(Mailbox &chan);  // Is it useful ? Maybe for workflow
+//void send(Mailbox &chan, void*payload, size_t simulatedSize);  // Is it useful ? Maybe for workflow
+        void kill();
+    };
+}}
 
-namespace this_task {
-
-};
-
-#endif
-
-class Eve {
-    public:
-        void operator()() {
-            ratioChange ratioFluctuation2[2] = {rC(0.0, 5), rC(40.0, 4)};
-            std::vector<ratioChange> ratioFluctuation(2);
-            ratioFluctuation.assign(&ratioFluctuation2[0], &ratioFluctuation2[0]+2);
-            this_task.setRatioVariation(ratioFluctuation);
-            sleep(99999999);
-            this_task.kill();
-        }
-};
-
-int main(int argc, char **argv) {
-    Engine *e = new Engine(&argc, argv);
-    e->loadPlatform("../../platforms/two_hosts.xml");
-    new ElasticTask("eve", Host::by_name("Tremblay"), Eve());
-    e->run();
-    return 0;
-}
+#endif //SIMGRID_ELASTIC_TASK_H
