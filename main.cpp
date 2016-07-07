4eb3b425f56b342eb7e85e2e277ed1e983ae903a
Simon Bihel <simon.bihel@outlook.com>
2016-07-07 17:11:52 +0200

    Week 8 Day 4

--- main.cpp
+++ main.cpp
@@ -7,23 +7,23 @@ XBT_LOG_NEW_DEFAULT_CATEGORY(s4u_test, "a sample log category");
 
 void eve(std::shared_ptr<simgrid::s4u::ElasticTaskManager> etm, int n) {
   XBT_INFO("Starting");
-  simgrid::s4u::ElasticTask *ets[n];
-  for(int i = 0; i < n; i++) {
-    ets[i] = new simgrid::s4u::ElasticTask(simgrid::s4u::Host::by_name("cb1-" + std::to_string(i+1)), 5.0, 1.0,
-                                           etm.get());
-  }
+  //simgrid::s4u::ElasticTask *ets[n];
+  //for(int i = 0; i < n; i++) {
+  //  ets[i] = new simgrid::s4u::ElasticTask(simgrid::s4u::Host::by_name("cb1-" + std::to_string(i+1)), 5.0, 1.0,
+  //                                         etm.get());
+  //}
   //simgrid::s4u::ElasticTask *e2 = new simgrid::s4u::ElasticTask(simgrid::s4u::Host::by_name("cb1-3"), 5.0, 0.0,
   //    etm.get());
   //e1->setOutputFunction([e2]() {
   //    e2->triggerOneTime(1.5);
   //});
-  //simgrid::s4u::ElasticTask *e3 = new simgrid::s4u::ElasticTask(simgrid::s4u::Host::by_name("cb1-4"), 5.0, 0.0,
-  //    etm.get());
-  //for(int i = 5; i < 2000; i++) {
-  //  e3->addHost(simgrid::s4u::Host::by_name("cb1-" + std::to_string(i)));
-  //}
-  //e3->setTimestampsFile("d20_timestamp_wc.txt");
-  simgrid::s4u::this_actor::sleep(100);
+  simgrid::s4u::ElasticTask *e3 = new simgrid::s4u::ElasticTask(simgrid::s4u::Host::by_name("cb1-4"), 1.0, 0.0,
+      etm.get());
+  for(int i = 5; i < 20000; i++) {
+    e3->addHost(simgrid::s4u::Host::by_name("cb1-" + std::to_string(i)));
+  }
+  e3->setTimestampsFile("d20_timestamp_wc.txt");
+  simgrid::s4u::this_actor::sleep(10000);
   etm->kill();
   XBT_INFO("Done.");
 }
